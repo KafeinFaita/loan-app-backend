63b6aae01b9884754dce2e15588d2e4d714271d3
Lynyrd Andres <57025300+KafeinFaita@users.noreply.github.com>
2023-02-24 22:34:08 +0800

    fixing auth bug

--- controllers/Sessions.js
+++ controllers/Sessions.js
@@ -6,8 +6,6 @@ class SessionController {
         try {
             const user = await User.login(req.body);
             req.session.user = user;
-            req.session.test = 1;
-            console.log(req.session)
             res.json(user);
         } catch (error) {
             throw error;  
@@ -20,7 +18,7 @@ class SessionController {
             if (req.session.user) {
                 return res.json(req.session.user);
             }
-            res.status(404).json({ error: "User not logged in." })
+            res.json({ error: "User not logged in." })
         } catch (error) {
             throw error;  
         }
