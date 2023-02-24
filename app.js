70fce4f41c2e78f630ef48d844b618bae1318fc4
Lynyrd Andres <57025300+KafeinFaita@users.noreply.github.com>
2023-02-24 23:31:32 +0800

    nothing works

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(session({
     cookie: { 
         httpOnly: true,
         maxAge: 30 * 24 * 60 * 60 * 1000,
-        sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax', // must be 'none' to enable cross-site delivery
+        // sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax', // must be 'none' to enable cross-site delivery
          // must be true if sameSite='none'
      },
      store: new MemoryStore({
