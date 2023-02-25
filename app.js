2f60cb115c6e6a9c2042faa020dafd54eaa9a21b
Lynyrd Andres <57025300+KafeinFaita@users.noreply.github.com>
2023-02-25 12:19:46 +0800

    enabled proxy on session settings when in production mode]"

--- app.js
+++ app.js
@@ -7,15 +7,12 @@ const cors = require('cors');
 const routes = require('./routes');
 require('dotenv').config()
 
-app.enable('trust proxy');
-
 app.use(cors({
     origin: ['https://andres-loaning-app.onrender.com', 'https://easy-red-fox-boot.cyclic.app', 'http://localhost:5173'],
     credentials: true
 }));
 app.use(session({
-    proxy: true,
-    // name: 'loaning_session',
+    proxy: process.env.NODE_ENV === 'production' ? true : false,
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
@@ -23,12 +20,11 @@ app.use(session({
         httpOnly: true,
         maxAge: 30 * 24 * 60 * 60 * 1000,
         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-        secure: process.env.NODE_ENV
+        secure: process.env.NODE_ENV === 'production'
      }
 }));
 
 console.log(`This API is in production: ${process.env.NODE_ENV === 'production'}`)
-console.log('added trust proxy enable')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
